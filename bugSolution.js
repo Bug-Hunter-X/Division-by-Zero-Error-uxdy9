5476b22c0a2fb45ddaf8a43c52643eeade78928d
BugHunter-X <askzenai@gmail.com>
2024-12-16 21:46:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,1 @@
+function add(a, b) { return a + b; } function subtract(a, b) { return a - b; } function multiply(a, b) { return a * b; } function divide(a, b) { if (b === 0) { return Infinity; // Or return 0, throw an error, or handle it as needed } return a / b; }
